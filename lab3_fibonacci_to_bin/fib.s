7ac3d9b76b660f355037a3c5ab7d6df22abaa6c9
reyzeer <czarneckizbigniew0@gmail.com>
2017-05-30 04:37:36 +0200

    3 & 4 ok

--- lab3_fibonacci_to_bin/fib.s
+++ lab3_fibonacci_to_bin/fib.s
@@ -4,7 +4,7 @@ STDOUT = 1          # Wyjscia konsoli (plik)
 SYS_READ = 0        # fn - wczytywanie danych z pliku (np. z wejscia konsoli)
 SYS_WRITE = 1	    # fn - zapisanie danych do pliku (np. do pliku wyjscia konsoli)
 
-BUFFOR_SIZE = 4096   # Dlugosc bufora danych
+BUFFOR_SIZE = 2048   # Dlugosc bufora danych
 
 MASK_LETTER = 0x1f # maska litera
 MASK_DIGIT = 0x10  # maska cyfra
@@ -13,9 +13,6 @@ MASK_DIGIT = 0x10  # maska cyfra
 BUFFOR: .space BUFFOR_SIZE
 BUFFOR_TEMP: .space BUFFOR_SIZE # buffor tymczasowy do przechowywania drugiej liczby fibonnaciego
 BUFFOR_TEMP2: .space BUFFOR_SIZE # buffor tymczasowy do przechowywania drugiej liczby fibonnaciego
-
-FILE_NAME: .ascii "fib.bin"
-FILE_NAME_STR_LEN = . - FILE_NAME
 
 .text
 .global main
@@ -247,112 +244,12 @@ ret
 
 main:
 
-	#mov $8, %ah
-	#shr $8, %ax
-
-	#xor %rax, %rax
-	#xor %rdx, %rdx
-	#movb $10, %dl
-	#movb $107, %al
-	#divb %dl
-
 	# Wywolanie liczenia fibonacciego
-	#push $20000
-	#push $BUFFOR
-	#push $BUFFOR_SIZE
-	#call fibonacci
-	#cnwrite $BUFFOR, $BUFFOR_SIZE
-
-	mov $2, %rax		# sys_open
-	mov $FILE_NAME, %rdi
-	mov $2, %rsi		#flagi
-	syscall
-
-	mov %rax, %r8  # fd - file descriptor
-
-	mov $9, %rax		# sys_mmap
-	mov $0, %rdi		# adres - pobranie z pliku wejscia
-	mov $2048, %rsi		# rozmiar pliku
-	mov $3, %rdx		# prot - PROT_WRITE Pages may be written.
-	mov $1, %r10		# flags
-	mov $0, %r9		# offset 0
-	syscall
-
-	# wskazniki
-	mov %rax, %r8 # adres na mape 
-	mov $BUFFOR, %r9 # adres na buffor
-
-	# liczniki
-	mov $2048, %rcx
-	xor %r10, %r10
-
-	xor %rax, %rax
-	xor %rdx, %rdx 
-
-read_file:
-
-	cmp $0, %rcx
-	je program_end
-
-	xor %rax, %rax
-	movb (%rcx, %r8, 1), %al
-	cmp $0, %al
-	je zero
-
-	mov $16, %rdx
-	divb %dl
-
-	cmp $10, %al
-	jl digit_first
-
-	addb $'A', %al
-	subb $10, %al
-	jmp secend_byte
-
-digit_first:
-	addb $'0', %al
-
-	jmp secend_byte
-
-secend_byte:
-
-	cmp $10, %ah
-	jl digit_second
-
-	addb $'A', %ah
-	subb $10, %ah
-	jmp copy_to_rregs
-
-digit_second:
-	addb $'0', %ah
-
-copy_to_rregs:
-	
-	movb %al, %r12b
-	shr $8, %ax
-	movb %al, %r11b
-
-	jmp save_in_buffor
-
-zero:
-
-	movb $'0', %r11b
-	movb $'0', %r12b
-
-save_in_buffor:
-
-	movb %r12b, (%r10, %r9, 1)
-	inc %r10
-	movb %r11b, (%r10, %r9, 1)
-	inc %r10
-	
-	dec %rcx
-	
-	jmp read_file
-	
-program_end:
-
+	push $10
+	push $BUFFOR
+	push $BUFFOR_SIZE
+	call fibonacci
 	cnwrite $BUFFOR, $BUFFOR_SIZE
-	
-	sysexit
 
+program_end:
+	sysexit
