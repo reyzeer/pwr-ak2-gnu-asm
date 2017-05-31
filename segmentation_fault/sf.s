e4581f9ab7accea6f963eb9cdf260b5cfc62d408
reyzeer <czarneckizbigniew0@gmail.com>
2017-06-01 01:18:56 +0200

    Segmentation fault - stack

--- segmentation_fault/sf.s
+++ segmentation_fault/sf.s
@@ -1,7 +1,17 @@
 .text
 .global main
 
+.type myFunc @function
+myFunc:
+
+ret
+
 main:
+
+	# trzeba podniesc tyle ile sie odlozylo, inaczej segmentation fault
+	push %rax
+	call myFunc
+	pop %rax
 
 	# sysexit generuje segmentation fault
 	# instrukcja zwrot sterowania
