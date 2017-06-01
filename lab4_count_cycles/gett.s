15a011eb79e9680c380dbbfb7fa2c0adec3af43b
reyzeer <czarneckizbigniew0@gmail.com>
2017-06-01 09:00:57 +0200

    Get cycles - works

--- lab4_count_cycles/gett.s
+++ lab4_count_cycles/gett.s
@@ -3,15 +3,17 @@
 .type getT @function
 getT:
 
+	# w 64 bit argumenty w funkcji sa odkladane kolejno w rejestrach rdi, rsi, etc, tj. w systemowych
+
 	# pobranie argumentu odloznego na poziomie jezyka C
 	#mov 2*8(%rbp), %rdx # tutaj dorzuca cos C
 	#mov 1*8(%rbp), %r9
 	#mov 2*8(%rbp), %r9
 	#mov 3*8(%rbp), %r9
 	
-	
+		
 
-	cmp $0, %r9
+	cmp $0, %rdi
 	je getT_option1
 	jne getT_option2 
 
@@ -30,6 +32,6 @@ getT_option2:
 getT_to64:
 
 	shl $32, %rdx   # gorne bity
-        add %rdx, %rax  # dodwanie do rax	
+        add %rdx, %rax  # dodwanie do rax
 ret
 
--- lab4_count_cycles/main.c
+++ lab4_count_cycles/main.c
@@ -8,7 +8,16 @@ void main ()
 	unsigned long int t1, t2, result;
 	
 	t1 = getT(0);
+	printf("%s %lu \n", "Poczatkowo cykli: ", t1);
+	
+	int x = 0;
+	for (int i = 0; i < 1000000; i++) {
+		x += i;	
+	}
+	printf("%d: \n", x);
+
 	t2 = getT(1);
+	printf("%s %lu \n", "Koncowo cykli: ", t2);
 
 	result = t2 - t1;
 	printf("%s %lu \n", "Licza wykonanych cykli: ", result);
